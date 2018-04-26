8b3cf8474564907579753718107078845959df8d
alexandermeek <alexandermeek09@gmail.com>
2018-04-26 20:09:40 +0100

    Update SharedMemory to include locks.

--- src/SharedMemory.java
+++ src/SharedMemory.java
@@ -1,40 +1,59 @@
 import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SharedMemory {
-    private ReentrantLock lock = new ReentrantLock();
+    private Lock lock = new ReentrantLock();
+    private Condition notFull = lock.newCondition();
+    private Condition notEmpty = lock.newCondition();
     private int m,size;
-    private LinkedList<Integer> numbers = new LinkedList<Integer>();
+    private LinkedList<Integer> numbers = new LinkedList<>();
 
-    public SharedMemory(ReentrantLock lock, int m) {
+    public SharedMemory(int m) {
         this.m = m;
         this.size = 0;
-        this.lock = lock;
-    }
-
-    public int getSize() {
-        return numbers.size();
     }
 
     public int readData() {
+        lock.lock();
         return numbers.peekFirst();
     }
 
-    public int takeData() {
-        if (this.size > 0) {
+    public void doNothing() {
+        lock.unlock();
+    }
+
+    public int takeData() throws InterruptedException{
+        try {
+            while(size==0) {
+                notEmpty.await();
+            }
             int temp = numbers.getFirst();
             numbers.removeFirst();
             this.size--;
+            notFull.signalAll();
             return temp;
-        } else {
-            return null;
+        } finally {
+            lock.unlock();
         }
     }
 
-    public void addData(int data) {
-        if (this.size < this.m) {
+    public void addData(int data) throws InterruptedException{
+        lock.lock();
+        try {
+            while (size == m) {
+                notFull.await();
+            }
             numbers.addLast(data);
             this.size++;
+            notEmpty.signalAll();
+        } finally {
+            lock.unlock();
         }
+    }
+
+    public String toString () {
+        return numbers.toString();
     }
 }
