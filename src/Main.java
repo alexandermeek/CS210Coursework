f827edd9687f0f3bdf3162b746a40f978c2800f4
alexandermeek <alexandermeek09@gmail.com>
2018-04-27 10:49:54 +0100

    Add line counter for files to check the number of numbers written the files.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     private static final int B = ConsumerThread.B;
@@ -6,23 +11,54 @@ public class Main {
 
     private static final int m = 4;
     private static final int n = 100*m;
-    private static final int k = 2;
+    private static final int k = 3; //Can only be 2 or 3 at the moment.
 
     public static void main(String[] args) throws InterruptedException{
         SharedMemory sharedMemory = new SharedMemory(m, n);
         Thread threadA = new ProducerThread(sharedMemory, n);
         Thread threadB = new ConsumerThread(B, sharedMemory);
         Thread threadC = new ConsumerThread(C, sharedMemory);
-        Thread threadD = new ConsumerThread(D, sharedMemory);
+        Thread threadD;
+        if (k == 3) {
+            threadD = new ConsumerThread(D, sharedMemory);
+        }
 
         threadA.start();
         threadB.start();
         threadC.start();
-        threadD.start();
+        if (k == 3) {
+            threadD.start();
+        }
 
         threadA.join();
         threadB.join();
         threadC.join();
-        threadD.join();
+        if (k == 3) {
+            threadD.join();
+        }
+
+        int totalNumbersWrittenToFile = 0;
+        totalNumbersWrittenToFile += countLines("B.txt");
+        totalNumbersWrittenToFile += countLines("C.txt");
+        if (k == 3) {
+            totalNumbersWrittenToFile += countLines("D.txt");
+        }
+
+        System.out.println("Finished!, " + totalNumbersWrittenToFile + " numbers total were written to the files.");
+    }
+
+    private static int countLines(String filename) {
+        int lines = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            while (reader.readLine() != null) {
+                lines++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            return lines;
+        }
     }
 }
